7e43ee4a0547d351f0eb4eb82471ebf795ee2907
melvi-l <melvil.miglio@gmail.com>
2025-06-26 19:36:47 +0200

    refactor and improve html

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import "./App.css";
 
+import { Provider } from "react-redux";
+import store from "./store/store";
+
+import LayerSwitcher from "./components/LayerSwitcher";
+
+import "./mocks/map";
+
 function App() {
-  return (
-    <main>
-      <h1>Hello world</h1>
-    </main>
-  );
+    return (
+        <main>
+            <h1>Legacy layer selector</h1>
+            <Provider store={store}>
+                <LayerSwitcher />
+            </Provider>
+        </main>
+    );
 }
 
 export default App;
